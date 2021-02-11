bdbc5a58bdaa3cf1e72cd234eacf183cfd9443ba
foggadog <78202566+foggadog@users.noreply.github.com>
2021-02-11 08:12:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import skinny 
-import time 
+import skinny
+import time
 import os
 import binascii
 
-key='0x5ba8c7572ac2df5e1b474a91441abbc3'
-with open ("testfile.txt", "r") as original_file:
+key = '0x5ba8c7572ac2df5e1b474a91441abbc3'
+with open("testfile.txt", "r") as original_file:
     data = str.encode(original_file.read())
 
 print(data)
 
 hex_data = binascii.hexlify(data)
 print(hex_data)
-##data = original_file.read 
-p = skinny.SkinnyCipher(int(key,16)) 
+##data = original_file.read
+p = skinny.SkinnyCipher(int(key, 16))
 
 start = time.time()
 
 
-for i in range(0,100):
-  d = p.encrypt(int(hex_data,16))
-    w = p.decrypt(d)
+for i in range(0, 100):
+  d = p.encrypt(int(hex_data, 16))
+  w = p.decrypt(d)
 
-print(w)
-end = time.time() 
-##encrypted = p.encrypted(data)
+print(d)
+print(format(w, '#018x'))
+end = time.time()
 
-wdcode = str(w, 'ascii')
+
+wdcode = format(w, '#018x')[2:]
 print(wdcode)
-decrypted = binascii.unhexlify(wdcode)
-with open ("enc_file.enc, wb") as encrypted_file:
+decrypted = binascii.unhexlify(wdcode).decode('ascii')
 
+#print(decrypted)
+
+with open("enc_file.enc", "w") as encrypted_file:
+    encrypted_file.write(decrypted)
